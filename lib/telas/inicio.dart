d6da63c79dd79e60184d9aedaa2661ff78c15834
Arnoldo Silva <arnoldosilva@asdesktop.arnoldosilva.com.br>
2020-02-27 09:48:22 -0300

    Padrão Factory implantado para montar lista de vídeos

--- lib/telas/inicio.dart
+++ lib/telas/inicio.dart
@@ -1,3 +1,5 @@
+import 'package:clone_youtube/api.dart';
+import 'package:clone_youtube/model/video.dart';
 import 'package:flutter/material.dart';
 
 class Inicio extends StatefulWidget {
@@ -6,15 +8,44 @@ class Inicio extends StatefulWidget {
 }
 
 class _InicioState extends State<Inicio> {
+  _listarVideos(String q) {
+    Api api = Api();
+    return api.pesquisar(q);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Início',
-          style: TextStyle(fontSize: 25),
-        ),
-      ),
-    );
+    return FutureBuilder<List<Video>>(
+        future: _listarVideos('street fighter'),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+              break;
+            case ConnectionState.active:
+            case ConnectionState.done:
+              if (snapshot.hasData) {
+                return ListView.separated(
+                    itemBuilder: (contex, index) {
+                      return Column(
+                        children: <Widget>[
+                          Text('Texto qualquer' + index.toString())
+                        ],
+                      );
+                    },
+                    separatorBuilder: (context, index) => Divider(
+                          height: 2,
+                          color: Colors.red,
+                        ),
+                    itemCount: snapshot.data.length);
+              } else {
+                return Text('Nenhum dado a ser exibido');
+              }
+              break;
+          }
+        });
   }
 }
