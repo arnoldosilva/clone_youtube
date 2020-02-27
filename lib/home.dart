fee5289db6e8dd89b9dddbaa32973e731b78cbc3
Arnoldo Silva <arnoldosilva@asdesktop.arnoldosilva.com.br>
2020-02-27 14:54:35 -0300

    icone seu canal

--- lib/home.dart
+++ lib/home.dart
@@ -35,7 +35,7 @@ class _HomeState extends State<Home> {
           //color: Colors.grey,
         ),
         title: Image.asset(
-          'img/youtube2.png',
+          'img/youtube3.png',
           width: 98,
           height: 22,
         ),
